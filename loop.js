935b5c92c19300a695f1a87416b3c499fd1a7a19
abhi-jha <abhi_jha1995@rediffmail.com>
2018-01-08 19:52:13 +0530

    improved fizzbuzz

--- loop.js
+++ loop.js
@@ -7,6 +7,28 @@
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
 
-for( var i = 0; i<11 ; i++){
-  console.log(i);
+//var num = prompt('Enter a number : ')
+
+//for( var i = 0; i<11 ; i++){
+//  console.log(i);
+//}
+
+for ( i  = 1 ; i<=100;i++){
+  str  = ''
+  if(i%3===0)
+    str = str + "fizz";
+  if(i%5===0)
+    str = str + "buzz";
+  if(str==='')
+    console.log(i);
+  else
+    console.log(str);  
 }
+/*
+Exception: SyntaxError: missing ; before statement
+@Scratchpad/2:23
+*/
+/*
+Exception: ReferenceError: num is not defined
+@Scratchpad/2:23:5
+*/
