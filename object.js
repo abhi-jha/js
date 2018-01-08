0bd777042f5d7ea84491b221d06631359cf24411
abhi-jha <abhi_jha1995@rediffmail.com>
2018-01-09 00:58:07 +0530

    function and objects

--- object.js
+++ object.js
@@ -6,15 +6,11 @@
  * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
-function sayHello(name){
-  console.log(name);
-}
-for(i =0;i<10;i++)
-  sayHello(i);
-sayHello('Hi MArk');
-sayHello();
-console.log(typeof(sayHello()))
-/*
-Exception: ReferenceError: type is not defined
-@Scratchpad/4:16:1
-*/
+
+var myCoffee = {
+  flavour : "espresso",
+  temperature: "piping hot",
+  ounces: 100,
+  milk: true
+};
+
