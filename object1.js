96c13d4e0e3c081818552a8896db2b857de06c80
abhi-jha <abhi_jha1995@rediffmail.com>
2018-01-10 00:51:15 +0530

    creating objects

---
+++ object1.js
@@ -0,0 +1,21 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+'use strict';
+function Cat(name, color){
+  this.name = name;
+  this.color = color;
+}
+var cat = new Cat('Fluffy', 'White');
+
+console.log(cat)
+/*
+Exception: ReferenceError: display is not defined
+@Scratchpad/1:17:1
+*/
