d42f14d909f5cd4cab11d53974c4ee04f2b76ddd
abhi-jha <abhi_jha1995@rediffmail.com>
2018-02-02 22:54:29 +0530

    patterns init

--- constructor_pattern.js
+++ constructor_pattern.js
@@ -1,25 +1,23 @@
-var Task = function(name){
-	this.name = name;
+var Task = function(data){
+	this.name = data.name;
 	this.completed = false;
-	this.complete = function(){
-		console.log('completing task: '+this.name);
-		this.completed = true;
-	}
-	this.save = function(){
-		console.log('saving Task: '+ this.name);
-	}
 }
 
-var task1 = new Task('create a demo for constructors');
-var task2 = new Task('create a demo for modules');
-var task3 = new Task('create a demo for singletons');
-var task4 = new Task('create a demo for prototypes');
+
+Task.prototype.complete = function(){
+		console.log('completing task: '+this.name);
+		this.completed = true;
+}
+Task.prototype.save = function(){
+		console.log('saving Task: '+ this.name);
+}
 
 
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
+
+
+module.exports = Task;
+
+
 
 var k = function(param1, param2){
 	this.param1 = param1;
---
+++ main.js
@@ -0,0 +1,15 @@
+var Task = require('./constructor_pattern.js');
+var Repo = require('./taskRepository.js');
+
+var ta = new Task(Repo.get(1));
+
+var task1 = new Task('create a demo for constructors');
+var task2 = new Task('create a demo for modules');
+var task3 = new Task('create a demo for singletons');
+var task4 = new Task('create a demo for prototypes');
+
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
